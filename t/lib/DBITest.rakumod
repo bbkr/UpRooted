b8c3cea1d54a9e5a8dd527b731a346705b8a99ae
Paweł bbkr Pabian <bbkr@post.pl>
2022-07-11 01:52:29 +0200

    https://github.com/bbkr/UpRooted/issues/5 - Added Schema::MySQL tests.

---
+++ t/lib/DBITest.rakumod
@@ -0,0 +1,57 @@
+unit module DBITest;
+
+use Test;
+
+sub connect ( $driver ) is export {
+
+    sub env-name ( $which ) {
+        return ( 'UPROOTED', $driver.uc, $which.uc ).join( '_' );
+    }
+    
+    for 'host', 'port', 'user', 'password', 'database' {
+        plan skip-all => env-name( $_ ) ~ ' environment variable not set' unless defined %*ENV{ env-name( $_ ) };
+    }
+
+    try { require ::( 'DBIish' ) }
+    plan skip-all => 'DBIish module not installed.' if $!;
+    use DBIish;
+
+    my $connection = try {
+        DBIish.connect(
+            $driver.lc,
+            host => %*ENV{ env-name( 'host' ) },
+            port => %*ENV{ env-name( 'port' ) },
+            user => %*ENV{ env-name( 'user' ) },
+            password => %*ENV{ env-name( 'password' ) },
+            database => %*ENV{ env-name( 'database' ) }
+        )
+    };
+    plan skip-all => 'Connection not established.' if $!;
+
+    return $connection;
+}
+
+sub load ( $connection, $directory, $file ) is export {
+
+    for $*PROGRAM.parent.add( $directory ).add( $file ).lines -> $line {
+        state $query = '';
+
+        # skip empty lines
+        next unless $line.chars;
+
+        # skip comments,
+        # block comments are NOT supported
+        next if $line.starts-with( '-- ' );
+
+        # accumulate statement lines
+        $query ~= $line ~ "\n";
+
+        # statement is complete
+        if $line.ends-with( ';' ) {
+            $connection.execute( $query );
+            $query = '';
+        }
+
+    }
+
+}
---
+++ t/mysql/schema.sql
@@ -0,0 +1,103 @@
+-- root table
+CREATE TABLE `t1` (
+    `id` bigint unsigned not null,
+    `c_01` char( 1 ),
+    `c_02` varchar( 8 ),
+    `c_03` tinytext,
+    `c_04` text,
+    `c_05` mediumtext,
+    `c_06` longtext,
+    `c_07` tinyint,
+    `c_08` smallint,
+    `c_09` mediumint,
+    `c_10` bigint unsigned,
+    `c_11` int,
+    `c_12` decimal( 8, 2 ),
+    `c_13` float( 8, 2 ),
+    `c_14` double( 8, 2 ),
+    `c_15` tinyblob,
+    `c_16` blob,
+    `c_17` mediumblob,
+    `c_18` longblob,
+    PRIMARY KEY ( `id` )
+) Engine = InnoDB;
+
+-- not nullable single column relation
+CREATE TABLE `t2` (
+    `id` bigint unsigned not null,
+    `t1_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_id` ) REFERENCES `t1` ( `id` )
+) Engine = InnoDB;
+
+-- not nullable relations of different lengths,
+-- shorter one should be chosen
+CREATE TABLE `t3` (
+    `id` bigint unsigned not null,
+    `t1_id` bigint unsigned not null,
+    `t2_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_id` ) REFERENCES `t1` ( `id` ),
+    FOREIGN KEY ( `t2_id` ) REFERENCES `t2` ( `id` )
+) Engine = InnoDB;
+
+-- not nullable relations longer than nullable one,
+-- not nullable one should be chosen
+CREATE TABLE `t4` (
+    `id` bigint unsigned not null,
+    `t1_id` bigint unsigned null,
+    `t2_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_id` ) REFERENCES `t1` ( `id` ),
+    FOREIGN KEY ( `t2_id` ) REFERENCES `t2` ( `id` ),
+    KEY( `t1_id`, `t2_id` )
+) Engine = InnoDB;
+
+-- multi column relation
+CREATE TABLE `t5` (
+    `id` bigint unsigned not null,
+    `t4_t1_id` bigint unsigned not null,
+    `t4_t2_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t4_t1_id`, `t4_t2_id` ) REFERENCES `t4` ( `t1_id`, `t2_id` )
+) Engine = InnoDB;
+
+-- self loop
+CREATE TABLE `t6` (
+    `id` bigint unsigned not null,
+    `t1_id` bigint unsigned not null,
+    `t6_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_id` ) REFERENCES `t1` ( `id` ),
+    FOREIGN KEY ( `t6_id` ) REFERENCES `t6` ( `id` )
+) Engine = InnoDB;
+
+-- long loop
+CREATE TABLE `t7` (
+    `id` bigint unsigned not null,
+    `t1_id` bigint unsigned not null,
+    `t8_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_id` ) REFERENCES `t1` ( `id` )
+) Engine = InnoDB;
+CREATE TABLE `t8` (
+    `id` bigint unsigned not null,
+    `t1_id` bigint unsigned not null,
+    `t7_id` bigint unsigned not null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_id` ) REFERENCES `t1` ( `id` ),
+    FOREIGN KEY ( `t7_id` ) REFERENCES `t7` ( `id` )
+) Engine = InnoDB;
+ALTER TABLE `t7` ADD FOREIGN KEY ( `t8_id` ) REFERENCES `t8` ( `id` );
+
+-- horse riddle
+CREATE TABLE `t9` (
+    `id` bigint unsigned not null,
+    `t1_1_id` bigint unsigned null,
+    `t1_2_id` bigint unsigned null,
+    PRIMARY KEY ( `id` ),
+    FOREIGN KEY ( `t1_1_id` ) REFERENCES `t1` ( `id` ),
+    FOREIGN KEY ( `t1_2_id` ) REFERENCES `t1` ( `id` )
+) Engine = InnoDB;
+
+CREATE VIEW `v1` AS SELECT * FROM `t1`;
